f3cbeb4be2db942f1548387cee9694b1d1a6041e
thenavodianplatform <thenavodianplatform@gmail.com>
2025-02-13 00:40:17 +0530

    Update page.tsx

--- app/speakers/page.tsx
+++ app/speakers/page.tsx
@@ -171,32 +171,7 @@ export default function Speakers() {
 						</div>
 					</div>
 					{/*===== TEAM AREA ENDS =======*/}
-					{/*===== CTA AREA STARTS =======*/}
-					<div className="cta1-section-area d-lg-block d-block">
-						<div className="container">
-							<div className="row">
-								<div className="col-lg-10 m-auto">
-									<div className="cta1-main-boxarea">
-										<div className="timer-btn-area">
-										<Countdown />
-											<div className="btn-area1">
-												<Link href="https://book.thenavodianplatform.in" className="vl-btn1">Buy Ticket</Link>
-											</div>
-										</div>
-										<ul>
-											<li>
-												<Link href="/#"><img src="/assets/img/icons/calender1.svg" alt="" />23 Feburary 2025 - 12pm to 5pm	</Link>
-											</li>
-											<li className="m-0">
-												<Link href="/#"><img src="/assets/img/icons/location1.svg" alt="" />Secret Location In The Chandigarh</Link>
-											</li>
-										</ul>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					{/*===== CTA AREA ENDS =======*/}
+					
 					{/*===== CTA AREA STARTS =======*/}
 					<div className="cta1-section-area d-lg-none d-block">
 						<div className="container">
