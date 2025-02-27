f52e243aef8f0e512be0f4fd8fee92a912fda5d6
thenavodianplatform <thenavodianplatform@gmail.com>
2025-02-28 00:30:50 +0530

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -194,7 +194,7 @@ Stand-up Comedy, StoryTelling, Beatboxing, etc.</p>
 										</div>
 										<ul>
 											<li>
-												<Link href="/#"><img src="/assets/img/icons/calender1.svg" alt="" />23 Feburary 2025 - 12pm to 5pm	</Link>
+												<Link href="/#"><img src="/assets/img/icons/calender1.svg" alt="" />30 March 2025 - 12pm to 5pm	</Link>
 											</li>
 											<li className="m-0">
 												<Link href="/#"><img src="/assets/img/icons/location1.svg" alt="" />Secret Location In The Chandigarh</Link>
@@ -221,7 +221,7 @@ Stand-up Comedy, StoryTelling, Beatboxing, etc.</p>
 										</div>
 										<ul>
 											<li>
-												<Link href="/#"><img src="/assets/img/icons/calender1.svg" alt="" />23 Feburary 2025 - 12pm to 5pm</Link>
+												<Link href="/#"><img src="/assets/img/icons/calender1.svg" alt="" />30 March 2025 - 12pm to 5pm</Link>
 											</li>
 											<li className="m-0">
 												<Link href="/#"><img src="/assets/img/icons/location1.svg" alt="" />Secret Location In The Chandigarh</Link>
