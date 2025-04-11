69e35454c5be2329e861e1d81e19a65a00b39eff
thenavodianplatform <thenavodianplatform@gmail.com>
2025-04-11 23:30:05 +0530

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -159,7 +159,7 @@ export default function Contact() {
 										</div>
 										<ul>
 											<li>
-												<Link href="/#"><img src="/assets/img/icons/calender1.svg" alt="" />To Be Announced
+												<Link href="/#"><img src="/assets/img/icons/calender1.svg" alt="" />11 May 2025
 	</Link>
 											</li>
 											<li className="m-0">
@@ -186,7 +186,7 @@ export default function Contact() {
 										</div>
 										<ul>
 											<li>
-												<Link href="/#"><img src="/assets/img/icons/calender1.svg" alt="" />To Be Announced
+												<Link href="/#"><img src="/assets/img/icons/calender1.svg" alt="" />11 May 2025
 </Link>
 											</li>
 											<li className="m-0">
