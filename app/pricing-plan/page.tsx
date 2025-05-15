e16fe8650c281b1b55cd3cd2f6712fc0a2793871
thenavodianplatform <thenavodianplatform@gmail.com>
2025-05-16 00:33:24 +0530

    Update page.tsx

--- app/pricing-plan/page.tsx
+++ app/pricing-plan/page.tsx
@@ -46,7 +46,7 @@ export default function PricingPlan() {
 										<ul>
 											<li><img src="/assets/img/icons/check2.svg" alt="" />One Day Ticket</li>
 											<li><img src="/assets/img/icons/check2.svg" alt="" />Refreshment Available</li>
-											<li><img src="/assets/img/icons/check2.svg" alt="" />11 May 2025</li>
+											<li><img src="/assets/img/icons/check2.svg" alt="" />1 June 2025</li>
 											
 										</ul>
 										<div className="space28" />
@@ -64,7 +64,7 @@ export default function PricingPlan() {
 										<ul>
 											<li><img src="/assets/img/icons/check2.svg" alt="" />One Day Ticket</li>
 											<li><img src="/assets/img/icons/check2.svg" alt="" />Refreshment Available</li>
-											<li><img src="/assets/img/icons/check2.svg" alt="" />11 May 2025</li>
+											<li><img src="/assets/img/icons/check2.svg" alt="" />1 June 2025</li>
 											
 										</ul>
 										<div className="space28" />
@@ -91,10 +91,10 @@ export default function PricingPlan() {
 										</div>
 										<ul>
 											<li>
-												<Link href="/#"><img src="/assets/img/icons/calender1.svg" alt="" />30 March 2025 - 12pm to 5pm</Link>
+												<Link href="/#"><img src="/assets/img/icons/calender1.svg" alt="" />1 June 2025</Link>
 											</li>
 											<li className="m-0">
-												<Link href="/#"><img src="/assets/img/icons/location1.svg" alt="" />Secret Location In The Chandigarh</Link>
+												<Link href="/#"><img src="/assets/img/icons/location1.svg" alt="" />Chandigarh</Link>
 											</li>
 										</ul>
 									</div>
