775322dcb94d697fa797d9e1b2c77a497803de29
thenavodianplatform <thenavodianplatform@gmail.com>
2025-05-16 00:33:41 +0530

    Update page.tsx

--- app/pricing-plan/page.tsx
+++ app/pricing-plan/page.tsx
@@ -117,7 +117,7 @@ export default function PricingPlan() {
 										</div>
 										<ul>
 											<li>
-												<Link href="/#"><img src="/assets/img/icons/calender1.svg" alt="" />30 March 2025 - 12pm to 5pm</Link>
+												<Link href="/#"><img src="/assets/img/icons/calender1.svg" alt="" />1 June 2025</Link>
 											</li>
 											<li className="m-0">
 												<Link href="/#"><img src="/assets/img/icons/location1.svg" alt="" />Secret Location In The Chandigarh</Link>
